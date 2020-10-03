fbc57ea303915d9d99a5fcbb371f772552d28912
cur-114 <cursan0409@gmail.com>
2020-10-03 22:41:06 +0900

    fix

--- src/main/java/net/rezxis/mchosting/spigot/gui2/plugins/config/ConfigManagerMenu.java
+++ src/main/java/net/rezxis/mchosting/spigot/gui2/plugins/config/ConfigManagerMenu.java
@@ -71,7 +71,7 @@ public class ConfigManagerMenu extends GUIWindow {
 			if (page == 0) {
 				end = from + 49;
 			} else {
-				end = from + 50;
+				end = from + 48;
 			}
 			if (end > files.length - 1) {
 				end = files.length - 1;
